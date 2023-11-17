5a66e0f1c4ae898234c952beef355deaccde296d
dantibel <danil.andr.belov@gmail.com>
2023-11-17 21:35:22 +0200

    Removed Matr4 class
    - special matrices remain in Camera

--- include/matr.hpp
+++ include/matr.hpp
@@ -10,10 +10,10 @@ namespace mr
   template <ArithmeticT T, std::size_t N>
     class Matr;
 
-  template <ArithmeticT T>
-    class Matr4;
-
   // common aliases
+  template <ArithmeticT T>
+    using Matr4 = Matr<T, 4>;
+
   using Matr4f = Matr4<float>;
   using Matr4d = Matr4<double>;
   using Matr4i = Matr4<int>;
@@ -205,59 +205,6 @@ namespace mr
     public:
       inline static const Matr identity = _identity();
       std::array<RowT, N> _data;
-    };
-
-  // specialization for special 4x4 matrices
-  template <ArithmeticT T>
-    class [[nodiscard]] Matr4 : public Matr<T, 4>
-    {
-    private:
-      using MatrT = Matr<T, 4>;
-
-    public:
-      using RowT = MatrT::RowT;
-
-      // constructors
-      constexpr Matr4(RowT r0, RowT r1, RowT r2, RowT r3) noexcept
-        : MatrT(r0, r1, r2, r3) {}
-
-      constexpr Matr4(const std::array<RowT, 4> &arr) : MatrT(arr) {}
-
-      // copy semantics
-      constexpr Matr4(const Matr4 &) noexcept = default;
-      constexpr Matr4 & operator=(const Matr4 &) noexcept = default;
-
-      // move semantis
-      constexpr Matr4(Matr4 &&) noexcept = default;
-      constexpr Matr4 & operator=(Matr4 &&) noexcept = default;
-
-      constexpr ~Matr4() = default;
-
-      // special matrices
-      static constexpr Matr4 view(const Vec3<T> &position, const Vec3<T> &direction,
-                                  const Vec3<T> &up, const Vec3<T> right) noexcept {
-        return {
-          RowT{          right.x(),           up.x(),          direction.x(), 0},
-          RowT{          right.y(),           up.y(),          direction.y(), 0},
-          RowT{          right.z(),           up.z(),          direction.z(), 0},
-          RowT{-(position & right), -(position & up), (position & direction), 1}};
-      }
-
-      static constexpr Matr4 orthogonal(T r, T l, T t, T b, T n, T f) noexcept {
-        return {
-          RowT{      2 / (r - l),                 0,                 0, 0},
-          RowT{                0,       2 / (t - b),                 0, 0},
-          RowT{                0,                 0,       2 / (n - f), 0},
-          RowT{(r + l) / (l - r), (t + b) / (b - t), (f + n) / (n - f), 1}};
-      }
-
-      static constexpr Matr4 frutstum(T r, T l, T t, T b, T n, T f) noexcept {
-        return {
-          RowT{  2 * n / (r - l),                 0,                   0,  0},
-          RowT{                0,   2 * n / (t - b),                   0,  0},
-          RowT{(r + l) / (r - l), (t + b) / (t - b),   (f + n) / (n - f), -1},
-          RowT{                0,                 0, 2 * n * f / (n - f),  0}};
-      }
     };
 } // namespace mr
 

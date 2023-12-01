9f701e28e060f68b3fa0f1f58c5e85d324a8f705
github-action-benchmark <github@users.noreply.github.com>
2023-12-01 00:11:13 +0000

    add Benchmark (googlecpp) benchmark result for 3429e13f8966ee72643172c4276579fba24d9db1

---
+++ dev/bench/data.js
@@ -0,0 +1,140 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1701389473575,
+  "repoUrl": "https://github.com/4J-company/mr-math",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "73912760+cone-forest@users.noreply.github.com",
+            "name": "Michael Tsukanov",
+            "username": "cone-forest"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3429e13f8966ee72643172c4276579fba24d9db1",
+          "message": "Manual triggering enabled",
+          "timestamp": "2023-12-01T03:10:20+03:00",
+          "tree_id": "42e209790dede8cf38d769880913d87498368a19",
+          "url": "https://github.com/4J-company/mr-math/commit/3429e13f8966ee72643172c4276579fba24d9db1"
+        },
+        "date": 1701389473186,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_normalized",
+            "value": 1.9847604207686693,
+            "unit": "ns/iter",
+            "extra": "iterations: 359329081\ncpu: 1.9847344334482073 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_normalized",
+            "value": 1.976331023461957,
+            "unit": "ns/iter",
+            "extra": "iterations: 359797095\ncpu: 1.9755590300138475 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_normalized",
+            "value": 1.92334428281908,
+            "unit": "ns/iter",
+            "extra": "iterations: 364070979\ncpu: 1.923245576791773 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_normalized_fast",
+            "value": 1.0695017159503277,
+            "unit": "ns/iter",
+            "extra": "iterations: 654026514\ncpu: 1.0695104633021035 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_normalized_fast",
+            "value": 1.0703803745818075,
+            "unit": "ns/iter",
+            "extra": "iterations: 654444755\ncpu: 1.0703757569269534 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_normalized_fast",
+            "value": 1.0700858216721252,
+            "unit": "ns/iter",
+            "extra": "iterations: 654245351\ncpu: 1.0700212067689576 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_dot",
+            "value": 0.6244157029999826,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 0.6243623999999999 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_dot",
+            "value": 0.6195692830000041,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 0.6195621999999998 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_dot",
+            "value": 0.6197912250000002,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 0.6197473999999996 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_cross",
+            "value": 6.803445855726962,
+            "unit": "ns/iter",
+            "extra": "iterations: 102864028\ncpu: 6.803115856983558 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_cross",
+            "value": 6.8034330442384885,
+            "unit": "ns/iter",
+            "extra": "iterations: 102613417\ncpu: 6.803090866762576 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_cross",
+            "value": 6.802661096292551,
+            "unit": "ns/iter",
+            "extra": "iterations: 102844080\ncpu: 6.80259184583108 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_scalar_triple_product",
+            "value": 10.861353902926977,
+            "unit": "ns/iter",
+            "extra": "iterations: 64547183\ncpu: 10.861234641332057 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_scalar_triple_product",
+            "value": 10.860799745936411,
+            "unit": "ns/iter",
+            "extra": "iterations: 64619997\ncpu: 10.85997574404095 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_scalar_triple_product",
+            "value": 10.844884426776991,
+            "unit": "ns/iter",
+            "extra": "iterations: 64645121\ncpu: 10.84445491253701 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_matrix_multiplication",
+            "value": 3.256070057298523,
+            "unit": "ns/iter",
+            "extra": "iterations: 212226168\ncpu: 3.2559019771774786 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_matrix_multiplication",
+            "value": 3.280166676399344,
+            "unit": "ns/iter",
+            "extra": "iterations: 212892528\ncpu: 3.2799972200057645 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_matrix_multiplication",
+            "value": 3.2937083667850455,
+            "unit": "ns/iter",
+            "extra": "iterations: 212841778\ncpu: 3.293471829576616 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}

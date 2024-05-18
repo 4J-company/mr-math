7999438edea54d27cbf630c5f074669ab1f9ae0d
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-05-18 14:17:54 +0300

    Compilation fixes

--- mr-math/rot.hpp
+++ mr-math/rot.hpp
@@ -72,21 +72,21 @@ namespace mr {
 
         // angle in radians
         constexpr Rotation & operator+=(Pitch<T> angle_rad) noexcept {
-          //_data *= Matr4<T>::rotate(angle_rad, mr::Vec3f(right()));
+          _data *= Matr4<T>::rotate(angle_rad, {right()});
 
           return *this;
         }
 
         // angle in radians
         constexpr Rotation & operator+=(Yaw<T> angle_rad) noexcept {
-          //_data *= Matr4<T>::rotate(angle_rad, mr::Vec3f(up()));
+          _data *= Matr4<T>::rotate(angle_rad, {up()});
 
           return *this;
         }
 
         // angle in radians
         constexpr Rotation & operator+=(Roll<T> angle_rad) noexcept {
-          //_data *= Matr4<T>::rotate(angle_rad, mr::Vec3f(direction()));
+          _data *= Matr4<T>::rotate(angle_rad, {direction()});
 
           return *this;
         }

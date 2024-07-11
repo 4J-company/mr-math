ab63a22966f9c4fb0d2777a23589426f191d56e5
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-07-11 17:42:45 +0300

    Refactor CMake script
    Download latest version of package manager.
    Manage dependencies in a single file.
    Add option to download tests (no actual tests provided).
    Rename benchmark executable to mr-math-bench.

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,29 @@
+file(
+  DOWNLOAD
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.38.3/CPM.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
+  EXPECTED_HASH SHA256=cc155ce02e7945e7b8967ddfaff0b050e958a723ef7aad3766d368940cb15494
+)
+include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
+
+if (ENABLE_BENCHMARK)
+  CPMFindPackage(
+    NAME benchmark
+    GITHUB_REPOSITORY google/benchmark
+    VERSION 1.5.2
+    OPTIONS "BENCHMARK_ENABLE_TESTING Off"
+  )
+endif()
+
+if (ENABLE_TESTING)
+  CPMFindPackage(
+    NAME googletest
+    GITHUB_REPOSITORY google/googletest
+    VERSION 1.14.0
+  )
+endif()
+
+if(googletest_ADDED)
+  # enable c++11 to avoid compilation errors
+  set_target_properties(googletest PROPERTIES CXX_STANDARD 11)
+endif()
--- cmake/download_benchmark.cmake
+++
@@ -1,9 +0,0 @@
-set(CXX_STANDARD 11)
-
-CPMAddPackage(
-  NAME benchmark
-  GITHUB_REPOSITORY google/benchmark
-  VERSION 1.5.2
-  OPTIONS "BENCHMARK_ENABLE_TESTING Off"
-)
-

a9b817f85d21cabc9255465722b2b18df73300e9
Danil Belov <danil.andr.belov@gmail.com>
2025-07-07 14:34:28 +0300

    Add missing inline keywords

--- include/mr-math/packing.hpp
+++ include/mr-math/packing.hpp
@@ -41,37 +41,39 @@ inline namespace math {
 
 namespace details {
 
-  Vec2f sign_non_zero(const Vec2f& v)
+  inline Vec2f sign_non_zero(const Vec2f& v)
   {
     return Vec2f{v.x() >= 0, v.y() >= 0} * 2 - Vec2f{1};
   }
 
-  Vec2f oct_wrap(const Vec2f& v)
+  inline Vec2f oct_wrap(const Vec2f& v)
   {
     return (Vec2f{1} - std::abs(Vec2f{v.y(), v.x()})) * sign_non_zero(v);
   }
 
   PackedNorm32 quantize32(const Vec2f& norm)
+  inline PackedNorm32 quantize32(const Vec2f& norm)
   {
     const auto norm_trans = (norm * 0.5f + Vec2f{ 0.5 }) * 1023.f;
     const auto norm_masked = static_cast<Vec2u>(norm_trans) & 0x3FF;
     return norm_masked.x() << 10 | norm_masked.y();
   }
 
-  Vec2f dequantize32(const PackedNorm32& norm)
+  inline Vec2f dequantize32(const PackedNorm32& norm)
   {
     const mr::Vec2f norm_masked{ (norm >> 10) & 0x3FF, norm & 0x3FF };
     return norm_masked / 1023.0f * 2.0f - Vec2f{ 1.0f };
   }
 
   PackedNorm24 quantize24(const Vec2f& norm)
+  inline PackedNorm24 quantize24(const Vec2f& norm)
   {
     const auto norm_trans = (norm * 0.5f + Vec2f{ 0.5 }) * 511.f;
     const auto norm_masked = static_cast<Vec2u>(norm_trans) & 0x1FF;
     return PackedNorm24(norm_masked.x() << 9 | norm_masked.y());
   }
 
-  Vec2f dequantize24(const PackedNorm24& norm)
+  inline Vec2f dequantize24(const PackedNorm24& norm)
   {
     const uint32_t norm32 = static_cast<uint32_t>(norm);
     const mr::Vec2f norm_masked{ (norm32 >> 9) & 0x1FF, norm32 & 0x1FF };
@@ -79,13 +81,14 @@ namespace details {
   }
 
   PackedNorm16 quantize16(const Vec2f& norm)
+  inline PackedNorm16 quantize16(const Vec2f& norm)
   {
     const auto norm_trans = (norm * 0.5f + Vec2f{ 0.5 }) * 31.f;
     const auto norm_masked = static_cast<Vec2u>(norm_trans) & 0x1F;
     return norm_masked.x() << 5 | norm_masked.y();
   }
 
-  Vec2f dequantize16(const PackedNorm16& norm)
+  inline Vec2f dequantize16(const PackedNorm16& norm)
   {
     const mr::Vec2f norm_masked{ (norm >> 5) & 0x1F, norm & 0x1F };
     return norm_masked / 31.0f * 2.0f - Vec2f{ 1.0f };
@@ -93,7 +96,7 @@ namespace details {
 
   // octahedron encoding
 
-  Vec2f pack_oct_impl(const Norm3f& norm)
+  inline Vec2f pack_oct_impl(const Norm3f& norm)
   {
     auto oct_proj = Vec2f{norm.x(), norm.y()} / (std::abs(norm.x()) + std::abs(norm.y()) + std::abs(norm.z()));
     if (norm.z() < 0) {
@@ -102,7 +105,7 @@ namespace details {
     return oct_proj;
   }
 
-  Norm3f unpack_oct_impl(const Vec2f& norm) {
+  inline Norm3f unpack_oct_impl(const Vec2f& norm) {
     // TODO: add shader code for unpacking normals
     Vec3f vec{ norm, 1 - std::abs(norm.x()) - std::abs(norm.y())};
     if (vec.z() < 0) {

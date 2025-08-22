664c1db7355c4e7071a5e2781e8c11a36784972c
Danil Belov <danil.andr.belov@gmail.com>
2025-08-22 22:08:14 +0300

    Fix include path for installing mr-math as a dependency

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ from conan.api.conan_api import ConanAPI
 
 class MrMath(ConanFile):
     name = "mr-math"
-    version = "1.1.1"
+    version = "1.1.2"
     license = "MIT"
 
     description = "Linear algebra library for computer graphics"
